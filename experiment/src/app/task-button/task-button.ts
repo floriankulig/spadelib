6bdc081fd7d7c4c57479276c931e4df76a28aedd
floriankulig <florian.kulig@web.de>
2025-08-16 15:11:57 +0200

    feat: add PlusIcon component and update task button to support icon-only variant

--- experiment/src/app/task-button/task-button.ts
+++ experiment/src/app/task-button/task-button.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TaskLayout } from '../setup/task-layout/task-layout';
 import { MaterialButton } from './material-button/material-button';
 import { SpadeButton } from './spade-button/spade-button';
+import { PlusIcon } from './plus-icon/plus-icon';
 
 @Component({
   selector: 'app-task-button',
-  imports: [TaskLayout, MaterialButton, SpadeButton],
+  imports: [TaskLayout, MaterialButton, SpadeButton, PlusIcon],
   templateUrl: './task-button.html',
   styleUrl: './task-button.scss',
 })
