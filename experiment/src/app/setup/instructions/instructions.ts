a7c05f885424aa1dc7be852fbb25a70a0886d980
floriankulig <florian.kulig@web.de>
2025-08-17 15:57:17 +0200

    add instructions page

--- experiment/src/app/setup/instructions/instructions.ts
+++ experiment/src/app/setup/instructions/instructions.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-instructions',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './instructions.html',
-  styleUrl: './instructions.scss'
+  styleUrl: './instructions.scss',
 })
-export class Instructions {
-
-}
+export class Instructions {}
