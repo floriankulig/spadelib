cd723a2a0c792bb0580d1d2fe4dd9b57aa23fdd8
floriankulig <florian.kulig@web.de>
2025-08-17 14:39:33 +0200

    feat: add PlusIcon component with associated styles

--- experiment/src/app/task-button/plus-icon/plus-icon.ts
+++ experiment/src/app/task-button/plus-icon/plus-icon.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-plus-icon',
   templateUrl: './plus-icon.html',
+  styleUrls: ['./plus-icon.scss'],
 })
 export class PlusIcon {}
